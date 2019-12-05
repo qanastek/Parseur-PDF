0d8126ff936a100afaf846479c353792a4f2f027
qanastek <yanis.labrak@alumni.univ-avignon.fr>
2019-12-05 09:31:26 +0100

    Clear the code for the presentation

--- README.md
+++ README.md
@@ -6,18 +6,12 @@ Vous trouverez ci-dessous le fonctionnement de chaque programmes implémenté pe
 
 ### Extraction des nom des fichiers, titres et résumés:
 
-Aller à la racine du repertoire où ce trouve les différents fichiers .PDF et lancer le programme à l'aide de:
+Aller à la **racine** du repertoire où ce trouve les différents fichiers .PDF et lancer le programme à l'aide de:
 
 TXT Output:
 
 ```console
-user@user: python ../src/main/Extraction_Informations.py -t
+user@user: python ../src/main/Extraction_Informations.py
 ```
 
-XML Output:
-
-```console
-user@user: python ../src/main/Extraction_Informations.py -x
-```
-
-Vous trouverez par la suite un fichier **resultat.txt** ou **xml** à l'endroit où vous avez exécuté le programme.
+Vous trouverez par la suite un fichier **resultat.txt** à l'endroit où vous avez exécuté le programme.
--- src/main/Extraction_Informations.py
+++ src/main/Extraction_Informations.py
@@ -10,10 +10,6 @@ sys.setdefaultencoding('utf-8')
 
 from xml.etree.ElementTree import Element, SubElement, Comment, tostring
 
-if len(sys.argv) != 2:
-	sys.exit("Erreur argument manquant !")
-
-
 def convertPdfToTxt(splitted):
     	
 	for name in splitted[0:-1]:
@@ -41,48 +37,11 @@ def getResume(data):
 	# Delete characters which aren't Alpha and Space
 	splitted2 = re.sub('[^a-zA-Z ]+', '', splitted2[:5]) + splitted2[5:]
 
+	return splitted2
+
 def getTitle(data):
     	
 	return data.split('\n')[0]
-
-def getReferences(data):
-
-	if re.search("references",data):
-		rslt = data.split('references')[1]
-
-	elif re.search("References",data):
-		rslt = data.split("References")[1]
-
-	elif re.search("REFERENCES",data):
-		rslt = data.split("REFERENCES")[1]
-
-	# Get each line of the page
-	page = rslt.split("\n")
-
-	for line in page:
-
-		if line == "\n" or len(line) <= 15 or re.match("^[\[\]0-9\.\ \|]+$",line):
-			# print(line)
-			page.remove(line)
-
-	# .split("\n\n")[0]
-	return "\n".join(page)
-
-def getAuthors(data):
-
-	return data.split('\n')[1]
-
-def getConclusion(data):
-	return ""
-
-def getIntroduction(data):
-	return ""
-
-def getDiscution(data):
-	return ""
-
-def getCorps(data):
-	return ""
 
 os.system("rm *.txt")
 os.system("rm *.xml")
@@ -97,14 +56,6 @@ ls = "ls *.pdf"
 c = os.popen(ls).read()
 
 splitted = c.split("\n")
-
-# Read the args
-if sys.argv[1] == "-t":
-    out='txt'
-elif sys.argv[1] == "-x":
-    out='xml'
-else:
-    print("argument incorrect") 
 
 # Convert each PDF to TXT
 convertPdfToTxt(splitted)
@@ -123,64 +74,22 @@ for item in splitted[0:-1]:
 		# Documents Informations
 		fileName = nameFile
 		title = getTitle(data)
-		author = getAuthors(data)
 		resume = getResume(data)
-		bibliographie = getReferences(data)
-		conclusion = getConclusion(data)
-		introduction = getIntroduction(data)
-		discution = getDiscution(data)
-		corps = getCorps(data)
 
-		# If the user want to export as TXT
-		if out == "txt":
-    			
-			# Create the output file
-			os.system('touch resultat.txt')
+		# Create the output file
+		os.system('touch resultat.txt')
 
-			# Write informations inside the file
-			with open("resultat.txt", 'a') as res:
-				res.write("\n")
-				res.write("File name: " + str(fileName))
-				res.write("\n\n")
-				res.write("Authors: " + str(author))
-				res.write("\n\n")
-				res.write("Title: " + str(title))
-				res.write("\n\n")
-				res.write("Résumé: " + str(resume))
-				res.write("\n\n")
-				res.write("Biblio: " + str(bibliographie))
-				res.write("\n\n-------------------------------------------------------------\n")
-			res.close()
-
-		# If the user want to export as XML
-		elif out == "xml":
-    			
-			root = Element('article')
-
-			preamble = SubElement(root, 'preamble')
-			preamble.text = str(fileName)
-
-			titre = SubElement(root, 'titre')
-			titre.text = str(title)
-
-			auteur = SubElement(root, 'auteur')
-			auteur.text = str(author)
-
-			abstract = SubElement(root, 'abstract')
-			abstract.text = str(resume)
-
-			biblio = SubElement(root, 'biblio')
-			biblio.text = str(bibliographie)
-
-			xml = tostring(root)
-
-			# Create the output file
-			os.system('touch resultat.xml')
-
-			# Write the XML object inside
-			with open("resultat.xml", 'a') as res:
-				res.write(xml)
-			res.close()
+		# Write informations inside the file
+		with open("resultat.txt", 'a') as res:
+			res.write("\n")
+			res.write("File name: " + str(fileName))
+			res.write("\n\n")
+			res.write("Title: " + str(title))
+			res.write("\n\n")
+			res.write("Résumé: " + str(resume))
+			res.write("\n\n")
+			res.write("\n\n-------------------------------------------------------------\n")
+		res.close()
 
 
 	f.close()
